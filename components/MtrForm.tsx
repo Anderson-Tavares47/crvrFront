2bf93bf69c68c3ff91263d1c52d8eefa7dd9b5bd
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2025-07-11 11:16:21 -0300

    Update MtrForm.tsx

--- components/MtrForm.tsx
+++ components/MtrForm.tsx
@@ -259,7 +259,7 @@ export default function MtrForm() {
     footerY += lineHeight;
 
     doc.setFontSize(16);
-    doc.text("Assinatura do Motorista: _______________________________________________________________________________________________________", margin, footerY);
+    doc.text("Assinatura do Motorista: _____________________________________________________________________________", margin, footerY);
     footerY += lineHeight;
     doc.text(`Data: ${dataFormatada}, ${horaFormatada}:${minutosFormatado}`, margin, footerY);
 
