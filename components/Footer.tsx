fbe1bded257e407d69e08aee36d372865adfb18a
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2025-08-18 22:44:43 -0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,10 +2,12 @@
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-200 text-center py-3 mt-10">
-      <p className="text-sm text-gray-600">
-        © {new Date().getFullYear()} Consulta MTR
-      </p>
+    <footer className="bg-[#293f58] text-white py-4 mt-10">
+      <div className="max-w-6xl mx-auto px-4 text-center">
+        <p className="text-sm">
+          © {new Date().getFullYear()} SIS Resíduos. Todos os direitos reservados.
+        </p>
+      </div>
     </footer>
-  );
+  )
 }
