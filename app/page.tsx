510f19dd282c23d285f8bc3d0d3e9e5cb9627b37
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2025-08-19 00:15:17 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,19 @@
 'use client'
 
-import Head from "next/head";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import MtrForm from "../components/MtrForm";
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Consulta de MTR</title>
-      </Head>
+  const router = useRouter();
 
-      <div className="flex flex-col min-h-screen">
-        <Header />
-
-        <main className="flex-1 p-6 flex flex-col items-center justify-center">
-          <h2 className="text-2xl font-semibold text-center">Buscar Manifesto de Transporte de Resíduos</h2>
-          <MtrForm />
-        </main>
-
-        <Footer />
-      </div>
-    </>
-  );
+  useEffect(() => {
+    const token = localStorage.getItem('authToken');
+    
+    if (!token) {
+      router.push('/login');
+    } else {
+      router.push('/dashboard');
+    }
+  }, [router]);
+  return null;
 }
