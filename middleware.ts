ce3b4810fe360f6de1c1f284d218f0cf717153b5
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2026-05-24 19:22:56 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,14 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl
+  // const { pathname } = request.nextUrl
 
-  // Permite acessar a própria página de bloqueio para não criar loop infinito
-  if (pathname === '/bloqueado') {
-    return NextResponse.next()
-  }
+  // // Permite acessar a própria página de bloqueio para não criar loop infinito
+  // if (pathname === '/bloqueado') {
+  //   return NextResponse.next()
+  // }
 
-  return NextResponse.redirect(new URL('/bloqueado', request.url))
+  // return NextResponse.redirect(new URL('/bloqueado', request.url))
 }
 
 export const config = {
@@ -17,6 +17,6 @@ export const config = {
     /*
      * Aplica o middleware em todas as rotas, exceto arquivos estáticos e internos do Next.js
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.jpg$|.*\\.svg$).*)',
+    // '/((?!_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.jpg$|.*\\.svg$).*)',
   ],
 }
